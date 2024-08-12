c4d2262f0ce2c3aeb9c8e1a5af7131714facdb5e
Steve Baker <steve.baker@bakerandboyle.com>
2024-08-12 16:12:20 +0100

    Remove unused import

--- src/test/java/com/bakerandboyle/library/infrastructure/BookRepositoryTest.java
+++ src/test/java/com/bakerandboyle/library/infrastructure/BookRepositoryTest.java
@@ -3,7 +3,6 @@ package com.bakerandboyle.library.infrastructure;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
 import java.util.Optional;
 
